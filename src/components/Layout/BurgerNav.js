46706e01af28ce5fc082e762eb354dfd79c56525
Will <Wi_llo@hotmail.com>
2020-02-28 16:24:00 +1100

    animated burger icon

--- src/components/Layout/BurgerNav.js
+++ src/components/Layout/BurgerNav.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './BurgerNavStyles/BurgerNavStyles.css';
 
-const BurgerNav = (props) => {
-    return (
-        <div className="BurgerNav">
-            <div className="BurgerNav__icon"></div>
-        </div>
-    )
+class BurgerNav extends Component {
+    state = {
+        navActive: false
+    }
+
+    handleClick = () => {
+        this.setState({
+            navActive: !this.state.navActive
+        });
+    }
+
+    render() {
+        return (
+            <div className={this.state.navActive ? 'BurgerNav BurgerNav--active' : 'BurgerNav'} onClick={this.handleClick}>
+                <div className={this.state.navActive ? 'BurgerNav__icon BurgerNav--active__icon' : 'BurgerNav__icon'}></div>
+            </div>
+        )
+    }
 };
 
 export default BurgerNav;
